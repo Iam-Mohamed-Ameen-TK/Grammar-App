60395eff5b489c07e9c0f4dbfafcf6ada52edcb0
Iam-Mohamed-Ameen-TK <mohamedameentk@gmail.com>
2025-11-12 22:31:08 +0530

    Initial Commit

---
+++ Grammar App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grammar App
+//
+//  Created by Mohamed Ameen on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grammar App/Grammar_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grammar_AppApp.swift
+//  Grammar App
+//
+//  Created by Mohamed Ameen on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Grammar_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
